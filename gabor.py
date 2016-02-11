9712f19fb5ab0e5890fde78a41d68b8db97becab
Charles Newey <charlie@assemblyco.de>
2016-02-11 19:38:58 +0000

    Visualise Gabor filter banks

--- gabor.py
+++ gabor.py
@@ -15,17 +15,10 @@ from scipy import ndimage as ndi
 
 from skimage import data
 from skimage.util import img_as_float
-from skimage.filters import gabor_kernel
+from skimage.filters import gabor_filter
 
 
 PATCH_SIZE = 80
-
-def compute_feats(image, kernel):
-    feats = np.zeros((2), dtype=np.double)
-    filtered = ndi.convolve(image, kernel, mode='wrap')
-    feats[0] = filtered.mean()
-    feats[1] = filtered.var()
-    return feats
 
 def power(image, kernel):
     # Normalize images for better comparison.
@@ -37,57 +30,58 @@ def power(image, kernel):
 image = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
 image_2 = cv2.imread(sys.argv[2], cv2.IMREAD_GRAYSCALE)
 
-locations = [(136, 237)]
-patches = []
-for loc in locations:
-    patches.append(image[loc[0]:loc[0] + PATCH_SIZE, loc[1]:loc[1] + PATCH_SIZE])
-    patches.append(image_2[loc[0]:loc[0] + PATCH_SIZE, loc[1]:loc[1] + PATCH_SIZE])
+loc = (136, 237)
+patch_1 = image[loc[0]:loc[0] + PATCH_SIZE, loc[1]:loc[1] + PATCH_SIZE]
+patch_2 = image_2[loc[0]:loc[0] + PATCH_SIZE, loc[1]:loc[1] + PATCH_SIZE]
 
-ckernels = []
-kern = gabor_kernel(0.4)
-for patch in patches:
-    ckernels.append(power(patch, kern))
+kerns_1 = []
+kerns_2 = []
+
+# append "real" part of array
+def gfri(gf):
+    return (np.real(gf[0]) + np.real(gf[1]))
+
+for theta in [0, 45, 135]:
+    gf = gabor_filter(patch_1, 0.2, theta=theta)
+    kerns_1.append((theta, gfri(gf)))
+    gf = gabor_filter(patch_2, 0.2, theta=theta)
+    kerns_2.append((theta, gfri(gf)))
 
 # create the figure
 fig = plt.figure(figsize=(8, 8))
 
 # display original image with locations of patchea
-ax = fig.add_subplot(3, 2, 1)
+ax = fig.add_subplot(4, 2, 1)
 ax.imshow(image, cmap=plt.cm.gray, interpolation='nearest', vmin=0, vmax=255)
-for (y, x) in locations:
-    ax.plot(x + PATCH_SIZE / 2, y + PATCH_SIZE / 2, 'gs')
+ax.plot(loc[1] + PATCH_SIZE / 2, loc[0] + PATCH_SIZE / 2, 'gs')
 ax.set_xlabel('Shadow')
 ax.set_xticks([])
 ax.set_yticks([])
 ax.axis('image')
 
-ax = fig.add_subplot(3, 2, 2)
+ax = fig.add_subplot(4, 2, 2)
 ax.imshow(image_2, cmap=plt.cm.gray, interpolation='nearest', vmin=0, vmax=255)
-for (y, x) in locations:
-    ax.plot(x + PATCH_SIZE / 2, y + PATCH_SIZE / 2, 'gs')
+ax.plot(loc[1] + PATCH_SIZE / 2, loc[0] + PATCH_SIZE / 2, 'gs')
 ax.set_xlabel('Non-shadow')
 ax.set_xticks([])
 ax.set_yticks([])
 ax.axis('image')
 
 # display the image patches
+patches = [patch_1, patch_2]
 for i, patch in enumerate(patches):
-    ax = fig.add_subplot(3, len(patches), i + 3)
+    ax = fig.add_subplot(4, len(patches), i + 3)
     ax.imshow(patch, cmap=plt.cm.gray, interpolation='nearest',
               vmin=0, vmax=255)
     ax.set_xlabel('Patch %s' % i)
 
 # display Gabor kernels
-for i, kernel in enumerate(ckernels):
-    ax = fig.add_subplot(3, 2, i + 5)
-    ax.imshow(np.real(kernel), cmap=plt.cm.gray, interpolation='nearest', vmin=0, vmax=255)
-    ax.set_xlabel('Patch %s' % i)
-
-
-# Check that the kernels aren't exactly the same
-a, b = (ckernels[0], ckernels[1])
-if np.equal(a, b).all():
-    print("Something went wrong")
+kernels = [kerns_1, kerns_2]
+for i, kerns in enumerate(kernels):
+    for j, (theta, kern) in enumerate(kerns):
+        ax = fig.add_subplot(4, 3, 7 + (3 * i) + j)
+        ax.imshow(kern, cmap=plt.cm.gray, interpolation='nearest', vmin=0, vmax=255)
+        ax.set_xlabel('Patch: %d, Theta: %d' % (i, theta))
 
 # display the patches and plot
 plt.show()
