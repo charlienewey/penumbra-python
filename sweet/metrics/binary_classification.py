6300f35339141a489f3911aa8a55c2b5e1c02e40
Charles Newey <charlie@assemblyco.de>
2016-04-09 02:31:03 +0100

    Fix FPR/FNR

--- sweet/metrics/binary_classification.py
+++ sweet/metrics/binary_classification.py
@@ -13,19 +13,6 @@ def _true_rate(ground_truth, prediction, target_value):
     return float(pred_num_true_target) / gt_num_target
 
 
-def _false_rate(ground_truth, prediction, target_value):
-    gt_target = ground_truth == target_value
-    gt_num_target = np.count_nonzero(np.bitwise_not(gt_target))
-
-    if gt_num_target == 0:
-        return 0.0
-
-    pred_true_target = prediction[gt_target] == target_value
-    pred_num_true_target = np.count_nonzero(np.bitwise_not(pred_true_target))
-
-    return float(pred_num_true_target) / gt_num_target
-
-
 def true_positive_rate(ground_truth, prediction):
     return _true_rate(ground_truth, prediction, 1)
 
@@ -35,8 +22,8 @@ def true_negative_rate(ground_truth, prediction):
 
 
 def false_positive_rate(ground_truth, prediction):
-    return _false_rate(ground_truth, prediction, 1)
+    return 1 - (_true_rate(ground_truth, prediction, 1))
 
 
 def false_negative_rate(ground_truth, prediction):
-    return _false_rate(ground_truth, prediction, 0)
+    return 1 - (_true_rate(ground_truth, prediction, 0))
