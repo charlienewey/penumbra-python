d198442c3aa42fa5c2c62be0fc44cf94202b39fa
Charles Newey <charlie@assemblyco.de>
2016-04-16 00:02:14 +0100

    Fix binary classification metrics (can now reduce multiple frames to a single number)

--- sweet/metrics/binary_classification.py
+++ sweet/metrics/binary_classification.py
@@ -1,30 +1,35 @@
 import numpy as np
 
-def _true_rate(ground_truth, prediction, target_value):
+def _match(ground_truth, prediction, target_value):
     gt_target = ground_truth == target_value
     gt_num_target = np.count_nonzero(gt_target)
-
-    # If there are no members of this class...
-    if gt_num_target == 0:
-        return 1.0
 
     pred_true_target = prediction[gt_target] == target_value
     pred_num_true_target = np.count_nonzero(pred_true_target)
 
-    return float(pred_num_true_target) / gt_num_target
+    return pred_num_true_target
+
+def _no_match(ground_truth, prediction, target_value):
+    gt_target = ground_truth == target_value
+    gt_num_target = np.count_nonzero(gt_target)
+
+    pred_false_target = prediction[gt_target] != target_value
+    pred_num_false_target = np.count_nonzero(pred_false_target)
+
+    return pred_num_false_target
 
 
-def true_positive_rate(ground_truth, prediction):
-    return _true_rate(ground_truth, prediction, 1)
+def true_positives(ground_truth, prediction):
+    return _match(ground_truth, prediction, 1)
 
 
-def true_negative_rate(ground_truth, prediction):
-    return _true_rate(ground_truth, prediction, 0)
+def true_negatives(ground_truth, prediction):
+    return _match(ground_truth, prediction, 0)
 
 
-def false_positive_rate(ground_truth, prediction):
-    return 1 - (_true_rate(ground_truth, prediction, 1))
+def false_positives(ground_truth, prediction):
+    return _no_match(ground_truth, prediction, 1)
 
 
-def false_negative_rate(ground_truth, prediction):
-    return 1 - (_true_rate(ground_truth, prediction, 0))
+def false_negatives(ground_truth, prediction):
+    return _no_match(ground_truth, prediction, 0)
